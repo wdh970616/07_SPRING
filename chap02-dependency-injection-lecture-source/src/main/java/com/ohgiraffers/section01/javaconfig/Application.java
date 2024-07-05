7e05b184298f4a628e424f4437d0611d63ad2fa6
wdh970616 <wdh970616@naver.com>
2024-07-05 10:53:51 +0900

    dependency injection

--- chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
@@ -1,7 +1,22 @@
 package com.ohgiraffers.section01.javaconfig;
 
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
 
+        // JAVA 성정 파일을 기반으로 ApplicationContext 객체 생성
+        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        MemberDTO member = context.getBean(MemberDTO.class);
+
+        System.out.println(member);
+        System.out.println(member.getPersonalAccount());
+        System.out.println(member.getPersonalAccount().deposit(100000000));
+        System.out.println(member.getPersonalAccount().getBalance());
+        System.out.println(member.getPersonalAccount().withDraw(50000000));
+        System.out.println(member.getPersonalAccount().getBalance());
     }
 }
--- chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/ContextConfiguration.java
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/ContextConfiguration.java
@@ -1,4 +1,22 @@
 package com.ohgiraffers.section01.javaconfig;
 
+import com.ohgiraffers.common.Account;
+import com.ohgiraffers.common.MemberDTO;
+import com.ohgiraffers.common.PersonalAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ContextConfiguration {
+
+    @Bean("account")
+    public Account accountGenerator() {
+        return new PersonalAccount(20, "110-234-56780");
+    }
+
+    @Bean("member")
+    public MemberDTO memberGenerator() {
+        // MemberDTO 생성자를 통해 Account를 생성하는 메소드를 호출하여 리턴값은 전달하여 bean을 조립할 수 있다.
+        return new MemberDTO(1, "홍길동", "010-1234-5678", "hong123@gmail.com", accountGenerator());
+    }
 }
