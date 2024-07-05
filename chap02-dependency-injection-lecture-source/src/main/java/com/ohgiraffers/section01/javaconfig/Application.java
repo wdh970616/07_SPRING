792a7942b177c6a3cf84377dc745e8478c1302e7
wdh970616 <wdh970616@naver.com>
2024-07-05 10:36:20 +0900

    dependency injection

---
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section01.javaconfig;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/ContextConfiguration.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section01.javaconfig;
+
+public class ContextConfiguration {
+}
