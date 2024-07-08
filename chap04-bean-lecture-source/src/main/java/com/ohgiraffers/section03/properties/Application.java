d0d60afffc3dbadaf8d8b84deae6df22d8d0af37
wdh970616 <wdh970616@naver.com>
2024-07-08 12:11:36 +0900

    bean

--- chap04-bean-lecture-source/src/main/java/com/ohgiraffers/section03/properties/Application.java
+++ chap04-bean-lecture-source/src/main/java/com/ohgiraffers/section03/properties/Application.java
@@ -1,6 +1,9 @@
 package com.ohgiraffers.section03.properties;
 
+import com.ohgiraffers.common.Beverage;
+import com.ohgiraffers.common.Bread;
 import com.ohgiraffers.common.Product;
+import com.ohgiraffers.common.ShoppingCart;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,7 +12,17 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
 
-        Product carpBread = context.getBean("carpBread", Product.class);
+        Product carpBread = context.getBean("carpBread", Bread.class);
         System.out.println("carpBread = " + carpBread);
+
+        Product milk = context.getBean("milk", Beverage.class);
+        Product water = context.getBean("water", Beverage.class);
+
+        ShoppingCart cart = context.getBean("cart", ShoppingCart.class);
+        cart.addItem(carpBread);
+        cart.addItem(milk);
+        cart.addItem(water);
+
+        System.out.println("cart에 담긴 상품들 : " + cart.getItems());
     }
 }
--- chap04-bean-lecture-source/src/main/java/com/ohgiraffers/section03/properties/ContextConfiguration.java
+++ chap04-bean-lecture-source/src/main/java/com/ohgiraffers/section03/properties/ContextConfiguration.java
@@ -1,11 +1,14 @@
 package com.ohgiraffers.section03.properties;
 
+import com.ohgiraffers.common.Beverage;
 import com.ohgiraffers.common.Bread;
 import com.ohgiraffers.common.Product;
+import com.ohgiraffers.common.ShoppingCart;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 
 import java.time.LocalDate;
 
@@ -13,14 +16,52 @@ import java.time.LocalDate;
 @PropertySource("section03/properties/product-info.properties")
 public class ContextConfiguration {
 
-    @Value("${bread.carpbread.name}")
+    /*
+     * 치환자(place-holder) 문법을 이용하여 properties에 저장된 key를 입력하면 value에 해당하는 값을 꺼내온다.
+     *
+     * 주의! 공백을 사용하면 값을 읽어오지 못한다.
+     * 콜론(:)을 이용하면 값을 읽어오지 못하는 경우 사용할 대체값을 작성할 수 있다.
+     */
+
+    @Value("${bread.carpbread.name:팥붕어빵}")
     private String carpBreadName;
 
-    @Value("${bread.carpbread.price}")
+    // 값은 여러번 불러와서 사용가능하다.
+//    @Value("${bread.carpbread.name:슈크림붕어빵}")
+//    private String carpBreadName2;
+
+    @Value("${bread.carpbread.price:0}")
     private int carpBreadPrice;
+
+    @Value("${beverage.milk.name:그냥우유}")
+    private String milkName;
+
+    @Value("${beverage.milk.price:0}")
+    private int milkPrice;
+
+    @Value("${beverage.milk.capacity:0}")
+    private int milkCapacity;
 
     @Bean
     public Product carpBread() {
         return new Bread(carpBreadName, carpBreadPrice, LocalDate.now());
+    }
+
+    @Bean
+    public Product milk() {
+        return new Beverage(milkName, milkPrice, milkCapacity);
+    }
+
+    @Bean
+    public Product water(@Value("${beverage.water.name:그냥물}") String waterName,
+                         @Value("${beverage.water.price:0}") int waterPrice,
+                         @Value("${beverage.water.capacity:0}") int waterCapacity) {
+        return new Beverage(waterName, waterPrice, waterCapacity);
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ShoppingCart cart() {
+        return new ShoppingCart();
     }
 }
